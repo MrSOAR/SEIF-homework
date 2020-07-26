fb621cd5f3f0bcfb44a02235584e0d2e3079272c
jeremy <jeremyncc94@hotmail.com>
2020-07-26 12:37:24 +0800

    removed file

--- demo.js
+++
@@ -1,7 +0,0 @@
-const givenNum = 23412;
-
-if (givenNum % 7 === 0) {
-  console.log(givenNum + " divisible by 7");
-} else {
-  console.log(givenNum + " NOT divisible by 7, remainder: " + (givenNum % 7));
-}
